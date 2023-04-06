ac28976363da40affafd77aa3ad2bba7206c058f
DcmodeX <dasadal.busy@gmail.com>
2023-04-06 01:24:20 -0700

    updating files

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log('connected')
+function getPetByStatus(){
+    const select=document.getElementById('pet-status')
+    console.log(select.name);
+    let url="https://petstore.swagger.io/v2/pet/findByStatus";
+    url=`${url}?${select.name}=${select.value} `
+    console.log(url)
+    fetch(url).then((res)=>res.json()).then((res)=>console.log(res))
+}
--- signup.js
+++ signup.js
@@ -1,11 +1,29 @@
-const form= document.getElementById('myForm');
-form.addEventListener('sibmit',(event)=>{
-    event.preventDefault;
-    const formElements=Array.from(form.elements);
-    const body={};
-    formElements.forEach((el)=>{
-        body[el.name]=value;
-    })
-})
+console.log('signup page is connected');
+const form = document.getElementById('userForm');
+
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+	const formElements = Array.from(form.elements);
+	const body = {};
+	formElements.forEach((el) => {
+		if (el.name) {
+			body[el.name] = el.value;
+		}
+	});
+	body.id = 0;
+	fetch('https://petstore.swagger.io/v2/user', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(body),
+	})
+		.then((res) => {
+			console.log(res.json());
+		})
+		.catch((err) => console.error(err));
+	console.log(JSON.stringify(body));
+});
+
 
 
